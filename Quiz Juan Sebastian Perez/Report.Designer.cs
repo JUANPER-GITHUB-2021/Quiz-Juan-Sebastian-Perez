ce1a166f96f47aec596b1a1bc1d197ec2eb43654
Juan Sebastian Pérez Millares <89596142+JUANPER-GITHUB-2021@users.noreply.github.com>
2021-08-27 22:59:00 -0500

    Final completo

--- Quiz Juan Sebastian Perez/Report.Designer.cs
+++ Quiz Juan Sebastian Perez/Report.Designer.cs
@@ -29,7 +29,28 @@ namespace Quiz_Juan_Sebastian_Perez
         /// </summary>
         private void InitializeComponent()
         {
+            this.Lblreport = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // Lblreport
+            // 
+            this.Lblreport.AutoSize = true;
+            this.Lblreport.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblreport.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Lblreport.Location = new System.Drawing.Point(370, 26);
+            this.Lblreport.Name = "Lblreport";
+            this.Lblreport.Size = new System.Drawing.Size(75, 26);
+            this.Lblreport.TabIndex = 0;
+            this.Lblreport.Text = "REPORT";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(263, 55);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(297, 329);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
             // 
             // Report
             // 
@@ -37,14 +58,20 @@ namespace Quiz_Juan_Sebastian_Perez
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Lblreport);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Report";
-            this.Text = "Form1";
+            this.Text = "Reporte";
             this.Load += new System.EventHandler(this.Report_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Lblreport;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
